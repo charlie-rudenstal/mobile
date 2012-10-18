ecc8c28ded4cbd27595af0cee5324fea54040ff0
charlie <charlie4@gmail.com>
2012-10-19 00:06:03 +0200

    Added real tv data

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,27 @@
+var request = require('request');
 
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+
+	// for regular caching and to make livejs behave nicely 
+	// (will refresh constantly with dynamic data otherwise)
+	if (!res.getHeader('Cache-Control')) {
+		res.setHeader('Cache-Control', 'public, max-age=' + (60 * 5));
+	}
+
+	var tableau = request('http://api.tv.nu/v2/numprograms/3', function(error, response, body) {
+
+		var channels = [];
+		try {
+			channels = JSON.parse(body).channels;
+		} catch (e) {}
+
+  		res.render('index', { title: 'tv.nu',
+  							  channels: channels });
+
+	});
+
 };
